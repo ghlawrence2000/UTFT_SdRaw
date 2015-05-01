8397e29e486e1691879894b3021626405735358a
Graham Lawrence <ghlawrence2000@yahoo.co.uk>
2015-05-01 23:34:50 +0100

    Hi speed image loading from SD to TFT
    A Due / Mega compatible replacement for UTFT_tinyFAT.
    
    Based on top of SdFat which now offers 128GB SDXC support, long
    filenames, and folder support!

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Just a SMALL update to remove the outdated #include <SdStream.h>
+
+Also added a statistics file showing the speed increase in the ARM 800x480 example as a result of the latest SdFat version.
+
+Thanks for downloading my UTFT_SdFat library, please give me some 'Karma' on the forum if you appreciate my efforts.
+
+Graham
---
+++ UTFT_SdRaw.h
@@ -0,0 +1,34 @@
+/*
+UTFT_SdRaw.cpp - Add-on library to integrate UTFT and SdFat.
+Copyright (C)2014 Graham Lawrence. Basic functionality of this library
+is based on UTFT_tinyFAT by Henning Karlsen. All right reserved
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the CC BY-NC-SA 3.0 license.
+Please see the included documents for further information.
+*/
+
+
+#ifndef UTFT_SdRaw_h
+#define UTFT_SdRaw_h
+#include <UTFT.h>
+#if ((!defined(UTFT_VERSION)) || (UTFT_VERSION<241))
+	#error : You will need UTFT v2.41 or higher to use this add-on library...
+#endif
+
+
+class UTFT_SdRaw
+{
+	public:
+		UTFT_SdRaw(UTFT *ptrUTFT);
+
+		int load(int x, int y, int sx, int sy, char *filename, int bufmult=1, bool iswap=0);
+		int loadcpld(int x, int y, int sx, int sy, char *filename, int writepage=0, int bufmult=1, bool iswap=0);
+		int pan(int x, int y, int sx, int sy, unsigned long offsetx, unsigned long offsety, unsigned long sizex, unsigned long sizey, char *filename, bool iswap=0);
+		// int panscale(int x, int y, int sx, int sy, unsigned long offsetx, unsigned long offsety, unsigned long sizex, unsigned long sizey, char *filename, byte scale);
+
+protected:
+		UTFT *_UTFT;
+};
+
+#endif
